1aa2eeae2a5be09794c1b51c2e2fba6d889d6765
mrpertama <kerndallp@gmail.com>
2025-05-26 11:02:10 +0800

    remove venv file

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello Worldzzzzz!'
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug='True')
