782d362c603871e5477acf172213fb4abc482f61
mrpertama <kerndallp@gmail.com>
2025-06-13 00:08:57 +0800

    added basic admin dashboard UI

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('admin_dashboard.html')
+    # Remember to change the render_template ah... I just testing my own side lol
+    # Idk how to fix the footer sia i try so hard but i scared sekali you guys need to adjust also
+    # Love, Kerndall
 
 
 if __name__ == '__main__':
